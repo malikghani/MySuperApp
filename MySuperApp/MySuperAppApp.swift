a2110cf62a9947ae063c6c50b28136b63d52e932
Ghani <hi@malikghani.com>
2023-07-21 21:42:57 +0700

    Initial Commit

---
+++ MySuperApp/MySuperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySuperAppApp.swift
+//  MySuperApp
+//
+//  Created by Ghani's Mac Mini on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MySuperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
